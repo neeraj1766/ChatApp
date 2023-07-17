85c02feeadff4a199e310f54ea6db1f541b9bf3c
neeraj1766 <114828194+neeraj1766@users.noreply.github.com>
2023-07-17 14:00:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "index.html"))
+    res.sendFile(path.resolve(__dirname1, "build" , "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
